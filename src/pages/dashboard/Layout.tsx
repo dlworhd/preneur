858dc4ec7978466705d161679df2ac5ffcccbec7
dlworhd <dlwodud821@gmail.com>
2025-06-04 14:58:51 +0900

    feat: 대시보드 레이아웃 시스템 구현

---
+++ src/pages/dashboard/Layout.tsx
@@ -0,0 +1,22 @@
+import "@/styles/globals.css";
+import "@/styles/dashboard.css";
+import Container from "@/components/dashboard/Container";
+import SideBar from "@/components/dashboard/Sidebar";
+import { cn } from "@/lib/utils";
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+    return (
+        <>
+            <SideBar />
+            <Container
+                className={cn(
+                    "border border-[var(--container-border)] rounded-md bg-[var(--container-background)]",
+                    "absolute top-[16px] right-[16px] w-[var(--container-width)] h-[var(--container-height)]"
+                )}
+            >
+                <Outlet />
+            </Container>
+        </>
+    );
+}
